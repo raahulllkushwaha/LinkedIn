0cfd4b54ae4bca2f7e753a6ca1ba55d868fcf51e
Rahul Kushwaha <raahullkushwaha@gmail.com>
2026-05-29 19:26:52 +0530

    Add connections service and setup the connections

---
+++ ConnectionsService/src/test/java/com/rahul/connectionsservice/ConnectionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.connectionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
