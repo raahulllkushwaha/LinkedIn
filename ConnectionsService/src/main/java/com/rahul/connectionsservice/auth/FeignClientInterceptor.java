68232b29ce78c56d02096c57fd58a4fc09698a73
Rahul Kushwaha <raahullkushwaha@gmail.com>
2026-05-30 12:21:10 +0530

    Add auth service in user and connection service

---
+++ ConnectionsService/src/main/java/com/rahul/connectionsservice/auth/FeignClientInterceptor.java
@@ -0,0 +1,18 @@
+package com.rahul.connectionsservice.auth;
+
+import com.rahul.postservice.auth.AuthContextHolder;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        Long userId = AuthContextHolder.getCurrentUserId();
+        if(userId != null) {
+            requestTemplate.header("X-User-Id", userId.toString());
+        }
+    }
+}
---
+++ UserService/src/main/java/com/rahul/userservice/auth/RequestInterceptor.java
@@ -0,0 +1,24 @@
+package com.rahul.userservice.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class RequestInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String userId = request.getHeader("X-User-Id");
+        if(userId != null) {
+            AuthContextHolder.setCurrentUserId(Long.valueOf(userId));
+        }
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        AuthContextHolder.clear();
+    }
+}
